1606d887a117f916233a64551c1905772a30cf91
Jeetendra chauhan <jeet4code@gmail.com>
2021-06-26 17:47:22 +0530

    render list

--- src/app/App.java
+++ src/app/App.java
@@ -1,9 +1,16 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class App {
     private static final String HELLO_WORLD = "Hello World";
-
+    private static List<String> list =  List.of("apple", "book", "cat");
     public static void main(String args[]) {
         System.out.println(HELLO_WORLD);
+
+        for (String item : list) {
+            System.out.println(item);
+        }
     }
 }
