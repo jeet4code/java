db54226b421e7c9b8fd3efbfd23205374d268407
Jeetendra chauhan <jeet4code@gmail.com>
2021-06-26 18:18:49 +0530

    use stream to render list

--- src/app/App.java
+++ src/app/App.java
@@ -1,16 +1,12 @@
 package app;
 
-import java.util.ArrayList;
 import java.util.List;
 
 class App {
     private static final String HELLO_WORLD = "Hello World";
-    private static List<String> list =  List.of("apple", "book", "cat");
+    private static List<String> list =  List.of("apple", "book", "cat", "dog", "elephant");
     public static void main(String args[]) {
         System.out.println(HELLO_WORLD);
-
-        for (String item : list) {
-            System.out.println(item);
-        }
+        list.stream().forEach(System.out::println);
     }
 }
