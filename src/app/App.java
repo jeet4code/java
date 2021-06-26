eb28916edc82e8c43c7182b645325236f9eb5e16
Jeetendra chauhan <jeet4code@gmail.com>
2021-06-26 17:01:15 +0530

    initial commit

---
+++ src/app/App.java
@@ -0,0 +1,9 @@
+package app;
+
+class App {
+    private static final String HELLO_WORLD = "Hello World";
+
+    public static void main(String args[]) {
+        System.out.println(HELLO_WORLD);
+    }
+}
